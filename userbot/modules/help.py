ccdc86122f7c82424301d75826bd2010b931a7bf
Wahyu Try <76894128+Callystaa@users.noreply.github.com>
2021-03-17 19:07:42 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -20,12 +20,12 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("**Module Salah Goblokkkk!!**")
+            await event.edit("**Modul Tidak Ditemukan!**")
             await asyncio.sleep(18)
             await event.delete()
     else:
         await event.edit(f"**╭━━━━━━━━━━━━━━━━━━━━━╮**\
-            \n│   Help for [🔥XBOT-REMIX🔥]\
+            \n│   Help for δDELTAδ\
             \n╰━━━━━━━━━━━━━━━━━━━━━╯ \
             \n╭━━━━━━━━━━━━━━━━━━━━━╮\
             \n│   Untuk melihat lengkap Command\
