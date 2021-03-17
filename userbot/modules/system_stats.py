78f917ee0d0ac5526793f13669bc1744bfce8f8c
Wahyu Try <76894128+Callystaa@users.noreply.github.com>
2021-03-17 20:00:58 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -233,17 +233,17 @@ async def amireallyalive(alive):
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
         f"۝⩵꙰ཱི►XBOT-REMIX◄⩵꙰ཱི۝\n running on 🤖 `{UPSTREAM_REPO_BRANCH}` 🤖\n"
-        f"╭━━━━━━━━━━━━━━━━━━━━━╮\n"
+        f"┏━━━━━━━━━━━━━━━━━━━━━\n"
         f"┣[•👤 `USER     :`{DEFAULTUSER}\n"
         f"┣[ 👁‍🗨 `Username :`@{user.username}\n"
-        "`┣▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱`\n"
+        "`┣━━━━━━━━━━━━━━━━━━━━`\n"
         f"┣[•⚙️ `Telethon :`v {version.__version__} \n"
         f"┣[•🐍 `Python   :`v {python_version()} \n"
         f"┣[•💻 `Base on  :`{UPSTREAM_REPO_BRANCH}\n"
         f"┣[•🛠 `Version  :`{BOT_VER} \n"
         f"┣[•🗃 `Modules  :`{len(modules)} Loaded\n"
         f"┣[•🕒 `Uptime   :`{uptime} \n"
-        f"╰━━━━━━━━━━━━━━━━━━━━━╯\n"
+        f"┗━━━━━━━━━━━━━━━━━━━━━\n"
         f" •MOD BY : `{DEFAULTUSER}`")
     if ALIVE_LOGO:
         try:
